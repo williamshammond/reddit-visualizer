f4f47c9762e9e3031a77023ee4717b6356a1b336
williamshammond <williamhammond44@gmail.com>
2020-10-30 10:01:37 -0700

    Update testpraw.py

--- testpraw.py
+++ testpraw.py
@@ -1,6 +1,8 @@
 import praw
 reddit = praw.Reddit(client_id="h6wy01n0-8H86A", client_secret="gezsDAlid5PVXPt6ug_nyqO2XKg2Lw",
-                     password="mccreeop4me", user_agent="macOS:myRedditVisualizer:v1.0(by u/whammmond)",
+                     password="Redditpassword2033!", user_agent="macOS:myRedditVisualizer:v1.0(by u/whammmond)",
                      username="whammmond")
 # Output score for the first 256 items on the frontpage
-reddit.subreddit("test").submit("Test Submission", url="https://reddit.com")
+# Output score for the first 256 items on the frontpage
+for submission in reddit.front.hot(limit=256):
+    print(submission.score)
