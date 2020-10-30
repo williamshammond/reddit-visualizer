58c6c89edba6f3daa0e895d6468bf8cf35a7dc18
williamshammond <williamhammond44@gmail.com>
2020-10-29 17:27:53 -0700

    Update testpraw.py

--- testpraw.py
+++ testpraw.py
@@ -1,1 +1,6 @@
-
+import praw
+reddit = praw.Reddit(client_id="h6wy01n0-8H86A", client_secret="gezsDAlid5PVXPt6ug_nyqO2XKg2Lw",
+                     password="mccreeop4me", user_agent="macOS:myRedditVisualizer:v1.0(by u/whammmond)",
+                     username="whammmond")
+# Output score for the first 256 items on the frontpage
+reddit.subreddit("test").submit("Test Submission", url="https://reddit.com")
